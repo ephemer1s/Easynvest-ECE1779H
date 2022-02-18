bb732b612445b7ce8043027cd0b81e18799e8e3e
HaozheWang <haozhe.wang.chn@outlook.com>
2022-02-18 01:26:21 -0500

    Database (#3)
    * Added comments, fixed APIs and stuffs
    
    * Delete .gitignore
    
    * Delete .idea directory
    
    * Delete .vscode directory
    
    * Delete app/__pycache__ directory
    
    * Added some functions in memcache api
    
    * Added some functions in memcache api
    
    * Getting rid of numOfItems since can just use len(dict.keys())
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Create config.py
    
    * Update run.py
    
    * add url for keylist
    
    * Create group.txt
    
    * Create keylist.html
    
    * modify keylist, add function get_db() teardown_db() connect_to_database()
    
    * Modifyed gitignore file
    
    * Modifyed gitignore file
    
    * Delete random.jpg
    
    Co-authored-by: StanderSam <samuel6304@gmail.com>
    Co-authored-by: Spica TSUKIMI <62980323+ephemer1s@users.noreply.github.com>
    Co-authored-by: ephemer1s <haochengvanadis@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,13 +1,35 @@
 from re import TEMPLATE
 
-
+import base64
 import os
 TEMPLATE_DIR = os.path.abspath("./templates")
 STATIC_DIR = os.path.abspath("./static")
 
-from flask import json, render_template, url_for, request
-from app import webapp, memcache
+from flask import json, render_template, url_for, request, g
+from app import webapp, memcache, memcacheStatistics, memcacheConfig
 
+
+
+import mysql.connector
+from app.config import db_config
+
+def connect_to_database():
+    return mysql.connector.connect(user=db_config['user'],
+                                   password=db_config['password'],
+                                   host=db_config['host'],
+                                   database=db_config['database'])
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@webapp.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 #===================================Under Construction=============================================
 @webapp.route('/')
@@ -26,8 +48,20 @@ def browse():
 
 
 @webapp.route('/keylist')
+# Display all keys in the database
 def keylist():
-    pass
+
+    cnx = mysql.connector.connect(user=db_config['user'],
+                                   password=db_config['password'],
+                                   host=db_config['host'],
+                                   database=db_config['database'])
+
+    cursor = cnx.cursor()
+    query = "SELECT * FROM keylist"
+    cursor.execute(query)
+    view = render_template("keylist.html",title="Keylist", cursor=cursor)
+    cnx.close()
+    return view 
 
 
 @webapp.route('/config')
@@ -39,6 +73,15 @@ def config():
 def status():
     pass
 #===================================Under Construction=============================================
+@webapp.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@webapp.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 @webapp.route('/get',methods=['POST'])
 def get():
@@ -60,7 +103,7 @@ def get():
 
     return response
 
-@webapp.route('/put',methods=['POST'])
+@webapp.route('/put', methods=['POST'])
 def put():
     key = request.form.get('key')
     value = request.form.get('value')
@@ -73,4 +116,3 @@ def put():
     )
 
     return response
-
