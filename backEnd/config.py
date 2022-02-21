fe8b237b59d17d1a18607c913fc77258dd3491e9
StanderSam <samuel6304@gmail.com>
2022-02-20 19:30:19 -0500

    Added file extension

--- backEnd/config.py
+++ backEnd/config.py
@@ -1,7 +1,8 @@
 class Config():
 
     MEMCACHE_FOLDER = './backEnd/cacheImageFolder'
-    IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff']
+    IMAGE_FORMAT = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.tif', '.bmp',
+                    '.raw', '.cr2', '.nef', '.orf', '.sr2', '.psd', '.xcf', '.ai', 'cdr']
     db_config = {'user': 'root',
                  'password': '19410524',
                  'host': 'database-1.clriqywhb6pw.us-east-1.rds.amazonaws.com',
