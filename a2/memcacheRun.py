001b09ba7fee92732eb1200a243a190bbc03f121
StanderSam <samuel6304@gmail.com>
2022-03-08 12:59:28 -0500

    Sam w/init (#32)
    * init for A2
    
    * Seperated memcache and the others into different run.py; memcache now runs on 5001

---
+++ a2/memcacheRun.py
@@ -0,0 +1,22 @@
+#!../venv/bin/python
+# from app import webapp
+# from app.main import main
+
+from werkzeug.serving import run_simple  # werkzeug development server
+# use to combine each Flask app into a larger one that is dispatched based on prefix
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from backEnd import webapp as backEnd
+
+backEnd.secret_key = "Secreeeeeeeeeeet"
+
+application = DispatcherMiddleware(backEnd)
+
+if __name__ == "__main__":
+    """Using "threaded = True", the function can call API within itself while dealing with user requests.
+    """
+    run_simple('0.0.0.0', 5001, application,
+               use_reloader=True,
+               use_debugger=False,
+               use_evalex=True,
+               threaded=True)
