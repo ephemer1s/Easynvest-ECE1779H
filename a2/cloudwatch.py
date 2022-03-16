d568381419210f80f741f37870041ba45967aee3
Spica TSUKIMI <62980323+ephemer1s@users.noreply.github.com>
2022-03-16 00:31:18 -0400

    Cloudwatch metrics (#41)
    * add cloudwatch api
    
    * Update .gitignore, add logs ignore
    
    * add writing log feature upon starting memcache on server
    
    * Create memcache.log

---
+++ a2/cloudwatch.py
@@ -0,0 +1,44 @@
+import boto3
+
+# Put custom metrics
+def putCacheMissRate(missrate, instance_name):
+    '''
+    Send Memcache Miss Rate to AWS Cloudwatch. Return a response message.
+    missrate: value to send
+    instance_name: current memcache instance identifier (could be anything based on your implementation)
+    '''
+    print('Sending Cache Miss Rate to Cloudwatch')
+    cloudwatch = boto3.client('cloudwatch')
+    response = cloudwatch.put_metric_data(
+        MetricData = [{
+                'MetricName': 'miss_rate',
+                'Dimensions': [{
+                        'Name': 'instance',
+                        'Value': instance_name
+                    }],
+                'Unit': 'percentage',
+                'Value': missrate}],
+        Namespace = 'ece1779/memcache')
+    print(response)
+    return response
+
+
+# Get list metrics through the pagination interface
+def getCacheMissRate(instances: list):
+    '''
+    Get miss rate from a specified server from cloudwatch. return a dict containing responses.
+    '''
+    cloudwatch = boto3.client('cloudwatch')
+    paginator = cloudwatch.get_paginator('list_metrics')
+    responses = {}
+    for i in instances:
+        response = paginator.paginate(
+            Dimensions=[{
+                'Name': 'instance',
+                'Value': i}],
+            MetricName='miss_rate',
+            Namespace='ece1779/memcache')
+        for content in response:
+            print(content['Metrics'])
+        responses[i] = response
+    return responses
--- a2/memcacheRun.py
+++ a2/memcacheRun.py
@@ -1,6 +1,7 @@
 #!../venv/bin/python
 # from app import webapp
 # from app.main import main
+import time, os
 
 from werkzeug.serving import run_simple  # werkzeug development server
 # use to combine each Flask app into a larger one that is dispatched based on prefix
@@ -19,8 +20,13 @@ application = DispatcherMiddleware({'/backEnd': backEnd})
 if __name__ == "__main__":
     """Using "threaded = True", the function can call API within itself while dealing with user requests.
     """
+    if not os.path.exists('./logs'):
+        os.mkdir('./logs')
+
     run_simple('0.0.0.0', 5001, application,
                use_reloader=True,
                use_debugger=True,
                use_evalex=True,
                threaded=True)
+    with open("./logs/memcache.log", 'a') as f:
+        f.write(str(time.time()) + "Memcache has loaded!\n")
