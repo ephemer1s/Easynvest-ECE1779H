e38207721f52bfb4c7d5d2ebbd2b87fd80702eb5
ephemer1s <haochengvanadis@gmail.com>
2022-03-20 04:34:46 -0400

    Add massive functions

--- a2/managerApp/showchart.py
+++ a2/managerApp/showchart.py
@@ -1,29 +1,131 @@
-import os
+import os, time
+import datetime as dt
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+### Data Example: [('2022-03-19 06:03', 0.15), ('2022-03-19 06:04', 0.29647), ('2022-03-19 06:05', 0.39)]
+class Chart(object):
+    x = None
+    y = None
+    style = {
+        'missrate': {
+            'title': 'Average Miss Rate',
+            'ylim': (0, 100),
+            'ylabel': 'percentage(%)',
+        },
+        'hitrate': {
+            'title': 'Average Hit Rate',
+            'ylim': (0, 100),
+            'ylabel': 'percentage(%)',
+        },
+        'totalrequest': {
+            'title': 'Total requests sent per minute',
+            'ylim': None,
+            'ylabel': 'requests',
+        },
+        'numofitems': {
+            'title': 'total number of items in MemCache',
+            'ylim': None,
+            'ylabel': 'items',
+        },
+        'totalsize': {
+            'title': 'total size of items in MemCache',
+            'ylim': None,
+            'ylabel': 'size (MB)',
+        },
+        'numofworkers': {
+            'title': 'number of workers',
+            'ylim': (0, 10),
+            'ylabel': 'worker num',
+        },
+    }
+    
+    def __init__(self, name,
+                 savepath='static/',
+                 figsize=(16, 9),
+                 ):
+        '''
+        Init the Chart object
+        name: (str) choose from 'missrate' | 'hitrate' | 'totalrequest' | 'numofitems' | 'totalsize' | 'numofworkers'
+        '''
+        self.fig = plt.figure(figsize=figsize)  # init figure
+        self.ax = self.fig.subplot(1, 1)
 
-def generate_seaborn_png(data, figname):
-    '''
-    generate chart for input data
-    data: 2D array (or dataframe), timestamp should be included
-    '''
-    # process data
-    assert isinstance(data, np.ndarray)
-    y = data[:, 0]
-    t = data[:, 1]
+        sns.set_style('whitegrid')  # define sns style
+        sns.set_context('poster')
 
-    # draw plot
-    fig = plt.figure()
-    ax = fig.subplot(1, 1)
-    sns.lineplot(x=t, y=y, ax=ax)
+        if not os.path.exists(savepath):  # Load save path
+            savepath = './managerApp/' + savepath
+        print('Using path ' + savepath)
+        self.savepath = savepath
 
-    # save figure
-    s = './static/'
-    if not os.path.isdir(s):
-        os.mkdir('./static')
-    savepath = s + figname
-    return plt.savefig(savepath)
+        if name in self.style:
+            self.name = name
+        else:
+            raise Exception('Unsupported Metric Name: {}'.format(name))
 
 
+    def load(self, raw_data):
+        '''
+        Load and preprocess the data before drawing plot
+        '''
+        x, y = [], []
+        for i in raw_data:
+            if len(i) is not 2:
+                raise Exception('Raw data have less than 2 elements in one of its datapoints')
+            else:
+                x.append(i[0])
+                y.append(i[1])
+        x = [dt.datetime.strptime(t, '%Y-%m-%d %H:%M') 
+                for t in x if isinstance(t, str)]  # force transform str -> timestamp only if str
+        self.x = np.array(x)
+        self.y = np.array(y)
+        return self
+
+
+    def percentage(self):
+        self.x = self.x * 100
+        return self
+
+
+    def plot(self):
+        '''
+        Plot a lineplot and a scatterplot with input data.
+        '''
+        if self.x is not None and self.y is not None:
+            sns.lineplot(self.x, self.y, ax=self.ax)
+            sns.scatterplot(self.x, self.y, markers='.', ax=self.ax)
+
+        else:
+            raise Exception('Failed to plot: X and Y not loaded')
+
+        self.ax.set(xlim=(x[-1] - dt.timedelta(minutes=30), x[-1]),
+                    title=self.style[self.name]['title'], 
+                    ylabel=self.style[self.name]['ylabel'])
+
+        if self.style[self.name]['ylim'] is not None:
+            self.ax.set(ylim=self.style[self.name]['ylim'])
+
+        return self
+
+
+    def save(self):
+        '''
+        Save figure drawed in self.fig
+        '''
+        path = self.savepath + self.name + '.png'
+        return self
+
+
+    def close(self):
+        '''
+        Close figure instance, release resources
+        '''
+        plt.close(self.fig)
+        return
+
+# Usage:
+if __name__ == '__main__':
+    data = [('2022-03-19 06:03', 0.15), ('2022-03-19 06:04', 0.29647), ('2022-03-19 06:05', 0.39)]
+    Chart('missrate').load(data).percentage().plot().save()
