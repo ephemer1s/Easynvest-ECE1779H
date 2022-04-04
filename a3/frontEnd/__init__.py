62266897e9a39710d31bd54cb462c850ec67dc71
ephemer1s <haochengvanadis@gmail.com>
2022-04-04 04:56:04 -0400

    Update __init__.py
    import main

--- a3/frontEnd/__init__.py
+++ a3/frontEnd/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
-from frontEnd import main
 from frontEnd.config import Config
 
 
 webapp = Flask(__name__)
+from frontEnd import main
+
+
+@webapp.before_first_request
+def _run_on_start():
+    """
+    App initialization, automatically run on start.
+    """
+    pass
+
