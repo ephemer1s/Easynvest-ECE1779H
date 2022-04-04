5f5468727af91e7861c1692a1f953927bb7fb846
ephemer1s <haochengvanadis@gmail.com>
2022-04-03 20:21:59 -0400

    Update run.py

--- a3/run.py
+++ a3/run.py
@@ -1,15 +1,16 @@
+from flask import Flask
 from werkzeug.serving import run_simple
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
-from backEnd import webapp as backEnd
 from frontEnd import webapp as frontEnd
+from backEnd import webapp as backEnd
 
-applications = DispatcherMiddleware(frontEnd, {'/backEnd': backEnd})
 
 
 if __name__ == "__main__":
     """Two Flask instances are combine into a single object. Using "threaded = True", the function can call API within itself while dealing with user requests.
     """
+    applications = DispatcherMiddleware(frontEnd, {'/backEnd': backEnd})
     run_simple('0.0.0.0', 5000, applications,
                use_reloader=True,
                use_debugger=False,
